3f0e92ffe3a6ee97f60e931765b017b4a56b19fb
alex <dashiev.2003@mail.ru>
2023-12-19 17:11:34 +0900

    CarClassExam

--- Test.php
+++ Test.php
@@ -1,229 +1,106 @@
 <?php
 
-class User
+class Car
 {
     private string $name;
-    private string $surname;
-    private int $phoneNumber;
-    private Basket $basket;
+    private string $color;
+    private string $model;
+    private float $price;
+    private Engine $engine;
 
+    public function __construct(string $name,string $color,string $model,float $price, Engine $engine)
+    {
+        $this->name = $name;
+        $this->color = $color;
+        $this->model = $model;
+        $this->price = $price;
+        $this->engine = $engine;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
 
     public function getName(): string
     {
         return $this->name;
     }
-    public function setName(string $name): void
+
+    public function setColor(string $color)
     {
-        $this->name = $name;
-    }
-    public function getSurname(): string
-    {
-        return $this->surname;
-    }
-    public function setSurname(string $surname): void
-    {
-        $this->surname = $surname;
-    }
-    public function getPhoneNumber(): int
-    {
-        return $this->phoneNumber;
-    }
-    public function setPhoneNumber(int $phoneNumber): void
-    {
-        $this->phoneNumber = $phoneNumber;
+        $this->color = $color;
     }
 
-    public function __construct(string $name, string $surname, int $phoneNumber)
+    public function getColor(): string
     {
-        $this->name = $name;
-        $this->surname = $surname;
-        $this->phoneNumber = $phoneNumber;
+        return $this->color;
     }
 
-    public function getBasket(): Basket
+    public function setModel(string $model)
     {
-        return $this->basket;
+        $this->model = $model;
     }
 
-    public function setBasket(Basket $basket): void
+    public function getModel(): string
     {
-        $this->basket = $basket;
+        return $this->model;
+    }
+
+    public function setPrice(float $price)
+    {
+        $this->price = $price;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function getEngine()
+    {
+        return $this->engine;
     }
 }
 
-
-class Good
+class ElectricalCar extends Car
 {
-    protected string $name;
-    protected float $cost;
+    private float $batteryCapacity;
 
-    public function __construct(string $name, float $cost)
+    public function __construct(string $name,string $color,string $model,float $price,float $batteryCapacity,Engine $engine)
     {
-        $this->name = $name;
-        $this->cost = $cost;
+        parent::__construct($name,$color,$model,$price,$engine);
+        $this->batteryCapacity = $batteryCapacity;
     }
 
-    public function getName()
+    public function getEnginePowering(): string
     {
-        return $this->name;
-    }
-
-    public function setName()
-    {
-        $this->name = $name;
-    }
-
-    public function setCost(float $cost): void
-    {
-        $this->cost = $cost;
-    }
-
-    public function getCost(): float
-    {
-        return $this->cost;
-    }
-
-    public function getDescription(): string
-    {
-        return $this->name . ' | ' . $this->cost . ' USD | ';
+        return "Charger";
     }
 }
 
-class Cloth extends Good
+
+class Engine
 {
-    private string $size;
+    private float $power;
 
-    public function __construct(string $name,float $cost,string $size)
+
+    public function __construct(float $power)
     {
-        parent::__construct($name, $cost);
-        $this->setSize($size);
+        $this->power = $power;
     }
 
-    public function getSize()
+    public function getPower(): float
     {
-        return $this->size;
-    }
-
-    public function setSize($size)
-    {
-        if (in_array($size, ["S", "M", "L"]))
-        {
-            $this->size = $size;
-        }
-        else
-        {
-            throw new Exception("Неверный размер одежды. Допустимые значения: S, M, L");
-        }
-    }
-    public function getDescription(): string
-    {
-        return "Cloth -> " . parent::getDescription() .'  '. $this->getSize() . " size" ."\n";
+        return $this->power;
     }
 }
+$engine1 = new Engine(100);
+$engine2 = new Engine(200);
+$car1 = new Car("BMW","Black","X5",50000,$engine1);
+$car2 = new ElectricalCar("Tesla","White","Model 3",70000,100,$engine2);
 
-
-
-class Product extends Good
-{
-    private int $expirationDate;
-
-    public function __construct(string $name, float $cost, int $expirationDate)
-    {
-        parent::__construct($name, $cost);
-        $this->expirationDate = $expirationDate;
-    }
-
-    public function getExpirationDate(): int
-    {
-        return $this->expirationDate;
-    }
-
-    public function setExpirationDate(int $expirationDate): void
-    {
-        $this->expirationDate = $expirationDate;
-    }
-    public function getDescription(): string
-    {
-        return "Product -> " . parent::getDescription() .' ' . $this->expirationDate . " days \n";
-    }
-}
-
-class Basket
-{
-    private $basket = [];
-
-    public function getArray(): array
-    {
-        return $this->basket;
-    }
-
-    public function addToBasketCloth(Cloth $cloth)
-    {
-        $this->basket[] = $cloth;
-    }
-
-    public function addToBasketProduct(Product $product)
-    {
-        $this->basket[] = $product;
-    }
-
-    public function removeFromBasket(int $index)
-    {
-        unset($this->basket[$index]);
-    }
-
-    public function getBasketTotalCost(): float
-    {
-        $totalCost = 0;
-        foreach ($this->basket as $item)
-        {
-            $totalCost += $item->getCost();
-        }
-        return $totalCost;
-    }
-
-    public function clearBasket(): void
-    {
-        $this->basket = [];
-    }
-}
-$user1 = new User("Ivan", "Ivanov", "+38012345678", []);
-
-$cloth1 = new Cloth("T-shirt", "10.99", "S");
-$cloth2 = new Cloth("Jeans", "25.99", "L");
-$cloth3 = new Cloth("Jacket", "50.99", "M");
-$cloth4 = new Cloth("Dress", "30.99", "S");
-$cloth5 = new Cloth("Skirt", "15.99", "L");
-
-$product1 = new Product("Bread", "2.99", "30");
-$product2 = new Product("Milk", "3.99", "30");
-$product3 = new Product("Eggs", "4.99", "30");
-$product4 = new Product("Butter", "5.99", "30");
-$product5 = new Product("Cheese", "6.99", "30");
-
-$basket1 = new Basket();
-
-$basket1->addToBasketCloth($cloth1);
-$basket1->addToBasketCloth($cloth2);
-$basket1->addToBasketCloth($cloth3);
-$basket1->addToBasketCloth($cloth4);
-$basket1->addToBasketCloth($cloth5);
-$basket1->addToBasketProduct($product1);
-$basket1->addToBasketProduct($product2);
-$basket1->addToBasketProduct($product3);
-$basket1->addToBasketProduct($product4);
-$basket1->addToBasketProduct($product5);
-$basket1->removeFromBasket(0);
-
-$user1->setBasket($basket1);
-
-foreach ($user1->getBasket()->getArray() as $item)
-{
-    echo $item->getDescription() . "<br>";
-}
-
-echo "Total Cost: " .$user1->getBasket()->getBasketTotalCost() . " USD \n";
-
+echo $car1->getEngine()->getPower();
 
 
 ?>
